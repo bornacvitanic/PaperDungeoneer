a2ece1060d81c0858508678d8daaaf735b53e063
Stjepan Knezevic <stjepanknezevic@fearem.com>
2025-02-01 14:39:25 +0100

    Modify TypingManager and TypingUI by cleaning up

--- Assets/PaperDungeoneer/Typing/TypingManager.cs
+++ Assets/PaperDungeoneer/Typing/TypingManager.cs
@@ -2,61 +2,65 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class TypingManager : MonoBehaviour
+namespace PaperDungeoneer.Typing
 {
-    [SerializeField] private TypingUI typingUI;
-    [SerializeField] private TMP_InputField inputField;
-    [Header("Colors")]
-    [SerializeField] private Color correctColor = Color.green;
-    [SerializeField] private Color incorrectColor = Color.red;
-    [SerializeField] private Color defaultColor = Color.white;
-
-    public UnityEvent OnWordCompleted;
-
-    private string targetWord;
-    public void Start()
+    public class TypingManager : MonoBehaviour
     {
-        inputField.text = "";
-        inputField.onValueChanged.AddListener(CheckInput);
-    }
+        [SerializeField] private TypingUI typingUI;
+        [SerializeField] private TMP_InputField inputField;
+        [Header("Colors")]
+        [SerializeField] private Color correctColor = Color.green;
+        [SerializeField] private Color incorrectColor = Color.red;
+        [SerializeField] private Color defaultColor = Color.white;
 
-    private void Update()
-    {
-        if (!inputField.isFocused)
-            inputField.ActivateInputField();
-    }
+        private string targetWord;
 
-    public void SetTargetWord(string word)
-    {
-        targetWord = word;
-        typingUI.SetTargetText(targetWord, defaultColor);
-        inputField.text = "";
-    }
+        public UnityEvent OnWordCompleted;
 
-    private void CheckInput(string currentInput)
-    {
-        typingUI.SetTargetText("", defaultColor);
-        for (int i = 0; i < targetWord.Length; i++)
+        public void Start()
         {
-            if (i < currentInput.Length)
-            {
-                if (currentInput[i] == targetWord[i])
-                    typingUI.AddLetterToTargetText(targetWord[i], correctColor);
-                else
-                    typingUI.AddLetterToTargetText(targetWord[i], incorrectColor);
-            }
-            else
-            {
-                typingUI.AddLetterToTargetText(targetWord[i], defaultColor);
-            }
+            inputField.text = "";
+            inputField.onValueChanged.AddListener(ProcessInput);
         }
 
-        if (IsWordCompleted(currentInput))
-            OnWordCompleted.Invoke();
-    }
+        private void Update()
+        {
+            if (!inputField.isFocused)
+                inputField.ActivateInputField();
+        }
 
-    public bool IsWordCompleted(string currentInput)
-    {
-        return currentInput == targetWord;
+        public void SetTargetWord(string word)
+        {
+            targetWord = word;
+            typingUI.SetTargetText(targetWord, defaultColor);
+            inputField.text = "";
+        }
+
+        private void ProcessInput(string currentInput)
+        {
+            typingUI.SetTargetText("", defaultColor);
+            for (int i = 0; i < targetWord.Length; i++)
+            {
+                if (i < currentInput.Length)
+                {
+                    if (currentInput[i] == targetWord[i])
+                        typingUI.AddLetterToTargetText(targetWord[i], correctColor);
+                    else
+                        typingUI.AddLetterToTargetText(targetWord[i], incorrectColor);
+                }
+                else
+                {
+                    typingUI.AddLetterToTargetText(targetWord[i], defaultColor);
+                }
+            }
+
+            CheckIfWordCompleted(currentInput);
+        }
+
+        public void CheckIfWordCompleted(string currentInput)
+        {
+            if(currentInput == targetWord)
+                OnWordCompleted.Invoke();
+        }
     }
 }
--- Assets/PaperDungeoneer/Typing/TypingUI.cs
+++ Assets/PaperDungeoneer/Typing/TypingUI.cs
@@ -1,26 +1,26 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class TypingUI : MonoBehaviour
+namespace PaperDungeoneer.Typing
 {
-    [Header("UI Elements")]
-    [SerializeField] private TMP_Text targetWordDisplay;
-
-    public void ResetTargetText()
+    public class TypingUI : MonoBehaviour
     {
-        targetWordDisplay.text = "";
-    }
+        [Header("UI Elements")]
+        [SerializeField] private TMP_Text targetWordDisplay;
 
-    public void SetTargetText(string targetedText, Color textColor)
-    {
-        targetWordDisplay.text = $"<color=#{ColorUtility.ToHtmlStringRGB(textColor)}>{targetedText}</color>";
-    }
+        public void ResetTargetText()
+        {
+            targetWordDisplay.text = "";
+        }
 
-    public void AddLetterToTargetText(char letter, Color letterColor)
-    {
-        targetWordDisplay.text += $"<color=#{ColorUtility.ToHtmlStringRGB(letterColor)}>{letter}</color>";
+        public void SetTargetText(string targetedText, Color textColor)
+        {
+            targetWordDisplay.text = $"<color=#{ColorUtility.ToHtmlStringRGB(textColor)}>{targetedText}</color>";
+        }
+
+        public void AddLetterToTargetText(char letter, Color letterColor)
+        {
+            targetWordDisplay.text += $"<color=#{ColorUtility.ToHtmlStringRGB(letterColor)}>{letter}</color>";
+        }
     }
 }
